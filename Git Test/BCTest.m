aef9d3ffa03093ce43c0d7ef163c6034e6294c3d
Bill Consoli <billconsoli@BC-Mac-Pro.local>
2014-03-03 13:01:53 -0600

    added a test class

---
+++ Git Test/BCTest.m
@@ -0,0 +1,13 @@
+//
+//  BCTest.m
+//  Git Test
+//
+//  Created by Bill Consoli on 3/3/14.
+//  Copyright (c) 2014 Bill Consoli. All rights reserved.
+//
+
+#import "BCTest.h"
+
+@implementation BCTest
+
+@end
